0443f218094eb422c5b17e3f8eb4db4ffafcc74c
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-03-24 16:49:39 -0700

    test

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Book = require('../lib/models/Book');
+const { insert } = require('../lib/models/Publisher');
 
 describe('bilbos-books routes', () => {
   beforeEach(() => {
@@ -14,53 +15,52 @@ describe('bilbos-books routes', () => {
   });
 
   it('creates a book', async () => {
-    const book = ({
+    const book = {
       title: 'Test',
       publisher_id: '90',
-      released: 1995
-    });
+      released: 1995,
+    };
 
-    const res = await request(app)
-      .post('/api/v1/books')
-      .send(book);
+    const res = await request(app).post('/api/v1/books').send(book);
 
     expect(res.body).toEqual({
-      book_id: expect.any(String), ...book });
+      book_id: expect.any(String),
+      ...book,
+    });
   });
 
   it('gets all books', async () => {
     const book = await Book.insert({
       title: 'Test',
       publisher_id: '90',
-      released: 1995
+      released: 1995,
     });
 
-    const res = await request(app)
-      .get('/api/v1/books');
+    const res = await request(app).get('/api/v1/books');
 
     expect(res.body).toEqual([
-      {  
-        book_id: '1', 
-        publisher_id: '1', 
-        released: 1922, 
-        title: 'hi' 
-      }, {
-        book_id: expect.any(String), ...book
-      }]);
+      {
+        book_id: '1',
+        publisher_id: '1',
+        released: 1922,
+        title: 'hi',
+      },
+      {
+        book_id: expect.any(String),
+        ...book,
+      },
+    ]);
   });
 
   it('gets a book by id', async () => {
     const book = await Book.insert({
-      title: 'Test',
-      publisher_id: 1,
-      released: 1995
+      title: 'Fight Club',
+      publisher_id: '1',
+      released: 2020,
     });
 
-    const res = await request(app)
-      .get(`/api/v1/books/${book.book_id}`);
+    const res = await request(app).get(`/api/v1/books/${book.book_id}`);
 
-    expect(res.body).toEqual({
-      book_id: book.book_id, ...book
-    });
+    expect(res.body).toEqual(book);
   });
 });
