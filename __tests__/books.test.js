d72b66bee9c0db114bb251bb40df0d47014ef31a
Benatovich <sambenatovich@gmail.com>
2022-03-24 16:02:38 -0400

    third test is a work in progress

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -48,4 +48,19 @@ describe('bilbos-books routes', () => {
         book_id: expect.any(String), ...book
       }]);
   });
+
+  it('gets a book by id', async () => {
+    const book = await Book.insert({
+      title: 'Test',
+      publisher_id: '90',
+      released: 1995
+    });
+
+    const res = await request(app)
+      .get(`/api/v1/books/${book.book_id}`);
+
+    expect(res.body).toEqual({
+      book_id: book.book_id, ...book
+    });
+  });
 });
