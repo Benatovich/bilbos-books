1df83f14f5935bf3043f0acb845fe5052ca0ab83
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-03-24 18:07:51 -0700

    workin on getReviews function in model Reviewer.

--- __tests__/reviewers.test.js
+++ __tests__/reviewers.test.js
@@ -15,62 +15,64 @@ describe('bilbos-books routes', () => {
   });
 
   it('creates a reviewer', async () => {
-    const reviewer = ({
+    const reviewer = {
       name: 'Ernie',
-      company: 'Good Books'
-    });
+      company: 'Good Books',
+    };
 
-    const res = await request(app)
-      .post('/api/v1/reviewers')
-      .send(reviewer);
+    const res = await request(app).post('/api/v1/reviewers').send(reviewer);
 
     expect(res.body).toEqual({ reviewer_id: expect.any(String), ...reviewer });
   });
 
-
   it('gets all reviewers', async () => {
     const reviewer = await Reviewer.insert({
       name: 'Olivia',
-      company: 'All the Books'
+      company: 'All the Books',
     });
 
-    const res = await request(app)
-      .get('/api/v1/reviewers');
+    const res = await request(app).get('/api/v1/reviewers');
 
-    expect(res.body).toEqual([{ 
-      reviewer_id: expect.any(String),
-      name: 'Denzel',
-      company: 'Most Books'
-    }, { ...reviewer }]);
-
+    expect(res.body).toEqual([
+      {
+        reviewer_id: expect.any(String),
+        name: 'Denzel',
+        company: 'Most Books',
+      },
+      { ...reviewer },
+    ]);
   });
 
   it('gets a reviewer by id', async () => {
     const reviewer = await Reviewer.insert({
       name: 'Sam',
-      company: 'No Books'
+      company: 'No Books',
     });
-    
+
     const review = await Review.insert({
       rating: 5,
       review: 'book slaps',
       book_id: '1',
       book_title: 'The Communist Manifesto',
-      reviewer_id: reviewer.reviewer_id
+      reviewer_id: reviewer.reviewer_id,
     });
 
-    const res = await request(app)
-      .get(`/api/v1/reviewers/${reviewer.reviewer_id}`);
+    const res = await request(app).get(
+      `/api/v1/reviewers/${reviewer.reviewer_id}`
+    );
 
-    expect(res.body).toEqual([{
-      reviewer_id: expect.any(String),
-      ...reviewer, ...review
-    }]);
+    expect(res.body).toEqual([
+      {
+        reviewer_id: expect.any(String),
+        ...reviewer,
+        ...review,
+      },
+    ]);
   });
 
   //   it('updates reviewer', async () => {
   //     const reviewer = await Reviewer.insert({
-  //       name: 'Ryan', 
+  //       name: 'Ryan',
   //       company: 'One Book'
   //     });
 
@@ -78,8 +80,4 @@ describe('bilbos-books routes', () => {
   //         .patch(`/api/v1/${reviewer.reviewer_id}`)
   //         .
   //   })
-
 });
-
-
-
